049830866dcc7bb6ea0cdea8e0c15212f13a5f12
gabrielmendes64 <gabriel10010100@gmail.com>
2024-12-14 18:47:06 -0300

    fix: change component for type Card

--- app/src/main/java/com/example/cardiosurgeryillustrator/ui/components/modules/SubjectCard.kt
+++ app/src/main/java/com/example/cardiosurgeryillustrator/ui/components/modules/SubjectCard.kt
@@ -1,39 +1,54 @@
 package com.example.cardiosurgeryillustrator.ui.components.modules
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.cardiosurgeryillustrator.R
 import com.example.cardiosurgeryillustrator.models.Subject
 import com.example.cardiosurgeryillustrator.ui.theme.Typography
+import com.example.cardiosurgeryillustrator.ui.theme.Zinc100
 import com.example.cardiosurgeryillustrator.ui.theme.Zinc800
 
 @Composable
 fun SubjectCard(modifier: Modifier = Modifier, subject: Subject, onClick: (Subject) -> Unit) {
-    Column (
+    Card (
         modifier = modifier,
+        onClick = { onClick(subject) },
+        colors = CardDefaults.cardColors(
+            containerColor = Color.Transparent
+        )
     ){
-        Row (
-            modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.Top,
-            horizontalArrangement = Arrangement.SpaceBetween,
+        Column (
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 16.dp),
         ){
-            Text(text = subject.title, style = Typography.headlineLarge)
-            Icon(
-                painter = painterResource(id = R.drawable.ic_arrow_right),
-                contentDescription = "Ícone flecha para direita",
+            Row (
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.Top,
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ){
+                Text(text = subject.title, style = Typography.headlineLarge)
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_arrow_right),
+                    contentDescription = "Ícone flecha para direita",
 
-            )
+                    )
+            }
+            Text(text = subject.description, style = Typography.bodyMedium, color = Zinc800)
         }
-        Text(text = subject.description, style = Typography.bodyMedium, color = Zinc800)
     }
 }
 
@@ -43,7 +58,7 @@ private fun SubjectCardPreview() {
     SubjectCard(
         modifier = Modifier.fillMaxWidth(),
         subject = Subject(
-            id = "dadadad",
+            id = "3",
             title = "Assunto 1",
             description = "Description duis aute irure dolor in reprehenderit in voluptate velit."
         ),
