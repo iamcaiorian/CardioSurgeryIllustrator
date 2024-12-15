41c982cac102ad5cb3ea3f9e4515246f39745409
iamcaiorian <caio.rianbr@gmail.com>
2024-12-15 19:07:22 -0300

    fix: remove unnecessary imports

--- app/src/main/java/com/example/cardiosurgeryillustrator/ui/components/patient/community/forum/LastForumMessage.kt
+++ app/src/main/java/com/example/cardiosurgeryillustrator/ui/components/patient/community/forum/LastForumMessage.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cardiosurgeryillustrator.R
-import com.example.cardiosurgeryillustrator.ui.theme.Blue100
 import com.example.cardiosurgeryillustrator.ui.theme.Blue700
 import com.example.cardiosurgeryillustrator.ui.theme.Typography
 
