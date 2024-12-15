d29f6a0e3dd455fe21419cb06e8fe7903d0d6072
iamcaiorian <caio.rianbr@gmail.com>
2024-12-15 11:58:22 -0300

    feat: add community models

---
+++ app/src/main/java/com/example/cardiosurgeryillustrator/models/patient/community/Comment.kt
@@ -0,0 +1,11 @@
+package com.example.cardiosurgeryillustrator.models.patient.community;
+
+data class Comment(
+    val id: String,
+    val topicId: String,
+    val userId: String,
+    val content: String,
+    val likes: Int,
+    val isCommentable: Boolean,
+    val timestamp: Long
+)
