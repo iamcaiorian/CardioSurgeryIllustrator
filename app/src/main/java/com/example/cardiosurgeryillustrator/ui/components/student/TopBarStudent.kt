d29b193b629d97a5bc4d3289dc15c89f1c071754
gabrielmendes64 <gabriel10010100@gmail.com>
2025-01-08 08:31:56 -0300

    feat: add module video screen and its components

--- app/src/main/java/com/example/cardiosurgeryillustrator/ui/components/student/TopBarStudent.kt
+++ app/src/main/java/com/example/cardiosurgeryillustrator/ui/components/student/TopBarStudent.kt
@@ -1,15 +1,23 @@
 package com.example.cardiosurgeryillustrator.ui.components.student
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.cardiosurgeryillustrator.navigation.TopBarStudentAction
+import com.example.cardiosurgeryillustrator.ui.theme.Typography
+import com.example.cardiosurgeryillustrator.ui.theme.Zinc900
 
 @ExperimentalMaterial3Api
 @Composable
@@ -24,18 +32,22 @@ fun TopBarStudent(navController: NavController) {
 
     TopAppBar(
         title = {
-            Row {
-                // Ícone do perfil e título alinhados à esquerda
+            Row (
+                modifier = Modifier,
+                Arrangement.spacedBy(16.dp),
+                verticalAlignment = Alignment.CenterVertically
+
+            ){
                 TopBarStudentAction.Profile.icon()
                 Text(
                     text = "Estudante",
-                    color = Color.Black
+                    style = Typography.headlineSmall,
+                    color = Zinc900
                 )
             }
         },
         actions = {
-            // Ícones da barra superior alinhados à direita
-            actions.drop(1).forEach { action -> // Ignorar o primeiro (Profile)
+            actions.drop(1).forEach { action ->
                 IconButton(
                     onClick = {
                         navController.navigate(action.route) {
@@ -50,4 +62,11 @@ fun TopBarStudent(navController: NavController) {
             }
         }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+private fun TopBarStudentPreview() {
+    TopBarStudent(navController = rememberNavController())
 }
