0325244f6a16a2e4dde17417d11e8f6331fb755e
EmanuelALemos <araujoemanuel97@gmail.com>
2025-02-21 00:55:41 -0300

    refactor: refactoring clickable points of heart

--- app/src/main/java/com/example/cardiosurgeryillustrator/ui/screens/patient/home/HomePatientScreen.kt
+++ app/src/main/java/com/example/cardiosurgeryillustrator/ui/screens/patient/home/HomePatientScreen.kt
@@ -4,22 +4,18 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -31,7 +27,6 @@ import androidx.navigation.compose.rememberNavController
 import com.example.cardiosurgeryillustrator.R
 import com.example.cardiosurgeryillustrator.models.mock.patient.diseases
 import com.example.cardiosurgeryillustrator.ui.components.buttons.StandardButton
-import com.example.cardiosurgeryillustrator.ui.modals.feedback.FeedbackModal
 
 @Composable
 fun HomePacientScreen(
@@ -45,7 +40,8 @@ fun HomePacientScreen(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Column (
@@ -64,14 +60,13 @@ fun HomePacientScreen(
                     contentDescription = "Heart illustration",
                     contentScale = ContentScale.Fit,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .size(300.dp)
+                        .size(700.dp)
                 )
 
                 selectedDisease.points.forEach { point ->
                     Box(
                         modifier = Modifier
-                            .size(24.dp)
+                            .size(32.dp)
                             .offset(x = point.xOffset, y = point.yOffset)
                             .clickable { navController.navigate(point.route) }
                     ) {
@@ -84,8 +79,6 @@ fun HomePacientScreen(
                 }
             }
         }
-
-        Spacer(modifier = Modifier.weight(1f))
 
         StandardButton(
             onClick = { navController.navigate("life_Style") },
@@ -104,6 +97,6 @@ private fun HomePacientScreenPreview() {
     HomePacientScreen(
         modifier = Modifier.fillMaxWidth(),
         navController = rememberNavController(),
-        diseaseIndex = 0
+        diseaseIndex = 7
     )
 }
